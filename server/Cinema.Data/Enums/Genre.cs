6629340fd6642998d40dd00e8d760ccdaedc824b
FraJimVit <francisco.jmz.vital@gmail.com>
2026-07-20 10:23:31 -0600

    Enum Genre language solved

--- server/Cinema.Data/Enums/Genre.cs
+++ server/Cinema.Data/Enums/Genre.cs
@@ -4,11 +4,11 @@ namespace Cinema.Data.Entities;
 
 public enum Genre
 {
-    [Description("Acción")] Action = 1,
-    [Description("Comedia")] Comedy = 2,
+    [Description("Action")] Action = 1,
+    [Description("Comedy")] Comedy = 2,
     [Description("Drama")] Drama = 3,
-    [Description("Terror")] Horror = 4,
-    [Description("Ciencia Ficción")] SciFi = 5,
+    [Description("Horror")] Horror = 4,
+    [Description("Science Fiction")] SciFi = 5,
     [Description("Romance")] Romance = 6,
-    [Description("Animación")] Animation = 7
+    [Description("Animation")] Animation = 7
 }
