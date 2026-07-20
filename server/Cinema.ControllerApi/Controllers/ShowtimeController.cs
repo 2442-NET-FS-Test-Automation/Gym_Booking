a131351280d784f50e22153d80ffe501cacda0c8
MasterDigimon <gamerdigimonxl@gmail.com>
2026-07-20 10:05:48 -0600

    Creating ShowtimeController, IshotimeService, ShowtimeService and ShowtimeDto files

---
+++ server/Cinema.ControllerApi/Controllers/ShowtimeController.cs
@@ -0,0 +1,14 @@
+using AutoMapper;
+using Cinema.ControllerApi.DTOs;
+using Cinema.ControllerApi.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
+
+
+[ApiController]
+[Route("api/[controller]")]
+public class ShowtimeController : ControllerBase
+{
+    
+}
---
+++ server/Cinema.ControllerApi/Services/ShowtimeService.cs
@@ -0,0 +1,10 @@
+using Cinema.ControllerApi.DTOs;
+using Cinema.Data.Entities;
+using Cinema.Data;
+
+namespace Cinema.ControllerApi.Services;
+
+public class ShowtimeService : IShowtimeService
+{
+    
+}
